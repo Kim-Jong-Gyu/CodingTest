43591dbfe27fa95e46ff7b2b2f8eef2058f9a24c
Kim-Jong-Gyu <whdrb2643@gmail.com>
2023-05-21 22:54:14 +0900

    feat: Make new logic

--- BAEKJOON/Bronze/2309.일곱난쟁이/p2309.cpp
+++ BAEKJOON/Bronze/2309.일곱난쟁이/p2309.cpp
@@ -1,5 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
+// 1. 순열 방식으로 접근
+int n = 9;
+int input[9];
+void solve()
+{
+    do
+    {
+        int sum = 0;
+        for (int i = 0; i < 7; i++)
+        {
+            sum += input[i];
+        }
+        if (sum == 100)
+            break;
+    } while (next_permutation(input, input + 9));
+    for (int i = 0; i < 7; i++)
+    {
+        cout << input[i] << '\n';
+    }
+    return;
+}
+
+int main()
+{
+    for (int i = 0; i < n; i++)
+    {
+        cin >> input[i];
+    }
+    sort(input, input + 9);
+    solve();
+}
+
+/*
+2. 조합을 통한 접근방식
 int n = 9;
 int input[9];
 int sum;
@@ -45,3 +79,4 @@ int main()
     }
     return 0;
 }
+*/
