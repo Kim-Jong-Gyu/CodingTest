98eba6f0209f87105f3329d7a8546198ad1fbc62
Kim-Jong-Gyu <whdrb2643@gmail.com>
2023-05-23 00:30:10 +0900

    feat: Add a new problem and new solution

---
+++ BAEKJOON/Bronze/1159.농구경기/Memo.md
@@ -0,0 +1,10 @@
+## 문제 
+https://www.acmicpc.net/problem/1159
+
+## 접근 방식
+1. Map을 아용한 counting
+    - map의 이터레이러를 통한 출력을 사용
+2. 배열을 이용한 counting
+    - 아스키 코드 사용
+        - char -> int : char타입 값 - 'a'
+        - int -> char : int타입 값 + 'a';
---
+++ BAEKJOON/Bronze/1159.농구경기/p1159.cpp
@@ -0,0 +1,52 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// 1. map을 이용한 counting
+// int num;
+// map<char, int> mp;
+// int main(){
+//     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+//     cin >> num;
+//     string name;
+//     int count = 0;
+//     for(int i = 0; i < num; i++){
+//         cin >> name;
+//         mp[name[0]] += 1;
+//     }
+//     for(auto it = mp.begin(); it != mp.end(); it++){
+//         if((*it).second >= 5){
+//             cout << (*it).first;
+//             count++;
+//         }
+//     }
+//     if(count == 0){
+//         cout << "PREDAJA";
+//     }
+//     return 0;
+// }
+
+#include <bits/stdc++.h>
+using namespace std;
+int cnt[26];
+int num;
+string name, str;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> num;
+    for (int i = 0; i < num; i++)
+    {
+        cin >> name;
+        cnt[name[0] - 'a']++;
+    }
+    for (int i = 0; i < 26; i++)
+    {
+        if (cnt[i] >= 5)
+            str += i + 'a';
+    }
+    if (str.size())
+        cout << str << '\n';
+    else
+        cout << "PREDAJA" << '\n';
+}
