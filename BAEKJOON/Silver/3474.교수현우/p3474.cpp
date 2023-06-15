b915e5439710f7438eae4c23064453462c80c3a0
Kim-Jong-Gyu <whdrb2643@gmail.com>
2023-06-15 22:25:34 +0900

    feat : Add a new problem and new solution

---
+++ BAEKJOON/Silver/3474.교수현우/p3474.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef unsigned long long ll;
+
+int t;
+ll num;
+
+int main()
+{
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        cin >> num;
+        int ret2 = 0;
+        int ret5 = 0;
+        for (int j = 2; j <= num; j *= 2)
+        {
+            ret2 += num / j;
+        }
+        for (int j = 5; j <= num; j *= 5)
+        {
+            ret5 += num / j;
+        }
+        cout << min(ret2, ret5) << '\n';
+    }
+}
