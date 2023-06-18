c05bd4f934a462113decf0c85d3562f237db2a29
Kim-Jong-Gyu <whdrb2643@gmail.com>
2023-06-19 01:38:08 +0900

    fix: fix the code for solving p2852 question

--- BAEKJOON/Silver/2852.NBA농구/p2852.cpp
+++ BAEKJOON/Silver/2852.NBA농구/p2852.cpp
@@ -1,137 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Team
-{
-    int teamN;
-    int teamH;
-    int teamS;
-};
+#define prev jong
 
 int n, tn;
-vector<Team> v;
-string ts;
-int score[2];
-Team t1;
-Team t2;
-pair<Team, Team> ret;
-Team prev;
+string st;
+int aT;
+int bT;
+int aS;
+int bS;
+string prev;
 
-Team timeSub(int comH1, int comS1, int comH2, int comS2)
+void printV(int result)
 {
-    Team tmpT;
-    if (comH1 > comH2)
-    {
-        tmpT.teamH = comH1 - comH2;
-        if (comS1 < comS2)
-        {
-            tmpT.teamH--;
-            tmpT.teamS = 60 + comS1 - comS2;
-        }
-        else
-        {
-            tmpT.teamS = comS1 - comS2;
-        }
-    }
-    else
-    {
-        tmpT.teamH = comH2 - comH1;
-        if (comS1 > comS2)
-        {
-            tmpT.teamH--;
-            tmpT.teamS = 60 + comS2 - comS1;
-        }
-        else
-        {
-            tmpT.teamS = comS2 - comS1;
-        }
-    }
-    return tmpT;
+    string m = "00" + to_string(result/60);
+    string s = "00" + to_string(result%60);
+    cout << m.substr(m.size() - 2, 2) <<  ":" + s.substr(s.size() - 2, 2) << '\n';
 }
 
-void printR()
-{
-    if (ret.first.teamH < 10)
-    {
-        cout << '0';
-    }
-    cout << ret.first.teamH << ":";
-    if (ret.first.teamS < 10)
-    {
-        cout << '0';
-    }
-    cout << ret.first.teamS << '\n';
-    if (ret.second.teamH < 10)
-    {
-        cout << '0';
-    }
-    cout << ret.second.teamH << ":";
-    if (ret.second.teamS < 10)
-    {
-        cout << '0';
-    }
-    cout << ret.second.teamS << '\n';
+int changeInt(string a){
+    return atoi(a.substr(0, 2).c_str()) * 60 + atoi(a.substr(3, 2).c_str());
 }
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+int go(int sumT, string nT){
+    int retT = 0;
+    retT = sumT + changeInt(nT) - changeInt(prev);
+    return retT;
+}
+
+int main(){
     cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> tn >> ts;
-        string tmph = "";
-        string tmps = "";
-        for (int i = 0; i < 2; i++)
-        {
-            tmph += ts[i];
+    for(int i = 0; i < n; i++){
+        cin >> tn >> st;
+        if(aS > bS){
+            aT = go(aT, st);
         }
-
-        for (int i = 0; i < 2; i++)
-        {
-            tmps += ts[i + 3];
+        if(aS < bS){
+            bT = go(bT, st);
         }
-        v.push_back({tn, stoi(tmph), stoi(tmps)});
+        if(tn == 1) aS++;
+        if(tn == 2) bS++;
+        prev = st;
     }
-    bool flag = 0;
-    for (auto it : v)
-    {
-        if (it.teamN == 1)
-            score[0]++;
-        if (it.teamN == 2)
-            score[1]++;
-        if (score[0] > score[1] && flag == 0){
-            t1.teamH = it.teamH;
-            t1.teamS = it.teamS;
-            flag = 1;
-        }
-        else if (score[0] < score[1] && flag == 0){
-            t2.teamH = it.teamH;
-            t2.teamS = it.teamS;
-            flag = 1; 
-        }
-        if(score[0] == score[1]){
-            flag = 0;
-            if(it.teamN == 2){
-                ret.first.teamH = it.teamH - t1.teamH;
-                ret.first.teamS = it.teamS - t1.teamS;
-            }
-            if(it.teamN == 1){
-                ret.second.teamH = it.teamH - t2.teamH;
-                ret.second.teamS = it.teamS - t2.teamS;
-            }
-        }
+    if(aS > bS){
+        aT = go(aT, "48:00");
     }
-
-    if (score[0] > score[1])
-    {
-        ret.first = timeSub(48, 00, t1.teamH, t1.teamS);
+    if(aS < bS){
+        bT = go(bT, "48:00");
     }
-    else if (score[0] < score[1])
-    {
-        ret.second = timeSub(48, 00, t2.teamH, t2.teamS);
-    }
-    printR();
+    printV(aT);
+    printV(bT);
 }
